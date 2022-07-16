600534b89fa09cfd805f63967e69e09255e07b7f
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-07-16 18:02:12 +0200

    Title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 function App() {
   return (
     <div className="App">
+      <h2 className="title" style={{marginTop: "2rem"}}>Upcoming Appointments</h2>
       <Main />
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <main>
+        <div className="container">
+            
+        </div>
+    </main>
   )
 }
 
