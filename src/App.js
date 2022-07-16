515b40951935b7050593ed176bbe3eccf0056671
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-07-16 17:56:21 +0200

    Set up Main.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import List from "./List";
-import data from "./data";
+import React from "react";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
-      <h2> App</h2>
+      <Main />
     </div>
   );
 }
