203431cebef4136f2904e2988088bd4c4cab7ebf
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-07-16 18:16:16 +0200

    Displayed appointment

--- src/List.js
+++ src/List.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-const List = () => {
-  return <ul></ul>;
+const List = (props) => {
+  return <ul className="user-list">
+    {
+      props.data.map((person) => (
+        <li key={person.id}>
+          <Person {...person} />
+        </li>
+      ))
+    }
+  </ul>;
 };
 
-const Person = () => {
-  return <article>Person</article>;
+const Person = ({id, name, state, img}) => {
+  return <article>
+    <img src={img} alt="prs" className="person-img" />
+    <div className="person-info">
+      <div className="person-action">
+        <h4>{name}</h4>
+        <button className="btn" onClick={console.log(id)}>X</button>
+      </div>
+      <p>{state}</p>
+    </div>
+  </article>;
 };
 
 export default List;
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 
 const Main = () => {
+
+    const [people, setPeople] = useState(data)
+
   return (
     <main>
         <div className="container">
-            
+            <div className="people-list">
+                <List data={people} />
+            </div>
         </div>
     </main>
   )
